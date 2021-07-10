df3cd7844a1645933559a262df1cb4b2289378aa
Jonathan <jonathan.barbero@gmail.com>
2021-07-10 20:00:06 -0300

    the contract

---
+++ counter-app/contracts/Counter.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.6;
+
+contract Counter {
+
+    uint256 public value;
+
+    event Increased(uint256 newValue);
+
+    function increase() public {
+        value = value + 1;
+        emit Increased(value);
+    }
+}
